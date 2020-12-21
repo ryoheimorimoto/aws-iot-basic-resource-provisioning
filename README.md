474cb9550ebe221fa0baea54fb32e2d168a9d2d6
ryoheimorimoto <ryoheimorimoto@gmail.com>
2020-12-21 18:26:34 +0900

    Add script to provision aws iot simple resource.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# aws-iot-basic-resource-provisioning
+# AWS IoT Core - Basic Resource Provisioning
+
+## About
+
+Provisioning of AWS IoT Resource in AWS Console is NOT so understandable and it is boring work. This script helps you to provision AWS IoT Core Resource easily.
+
+## Usage
+
+- Fix the `policy_template.json` file to appropriate your business application.
+- Fix the constants of `AWS_REGION` in `iot-resource-provisioning.sh` for your target AWS region.
+- Run command as followings.
+
+```
+$ sh iot-resource-provisioning.sh 00001
+```
+
+- After executing this command, you can find key-pairs and its client certificate in `00001` directory.
---
+++ iot-resource-provisioning.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+index=$1
+
+if [ ! $index ]; then
+  echo "Please input index value"
+  exit 0
+fi
+
+mkdir ${index}
+
+AWS_REGION="ap-northeast-1" # Overwrite for your region.
+
+echo "- - - - - - - - - - - - - - - - - - - -"
+echo "Start to create policy."
+
+policy_name="unvs_agre_proto_${index}_policy"
+policy_file_name="${index}/${index}-policy.json"
+sed -e "s/<<<INDEX>>>/${index}/g" ./policy_template.json > ${policy_file_name}
+
+policy_arn=`aws iot create-policy \
+  --policy-name ${policy_name} \
+  --policy-document "file://${policy_file_name}" \
+  --region ${AWS_REGION} \
+  | jq -r '.policyArn'`
+
+echo "Policy ARN: ${policy_arn}"
+
+echo "- - - - - - - - - - - - - - - - - - - -"
+echo "Start to create Thing."
+
+thing_name="unvs_agre_proto_${index}"
+
+thing_arn=`aws iot create-thing \
+  --thing-name ${thing_name} \
+  --region ${AWS_REGION} \
+  | jq -r '.thingArn'`
+
+echo "Thing ARN: ${thing_arn}"
+
+echo "- - - - - - - - - - - - - - - - - - - -"
+echo "Start to create certificate."
+
+certificate_arn=`aws iot create-keys-and-certificate \
+  --set-as-active \
+  --certificate-pem-outfile "${index}/${index}-client-certificate.pem" \
+  --public-key-outfile "${index}/${index}-public.key" \
+  --private-key-outfile "${index}/${index}-private.key" \
+  --region ${AWS_REGION} \
+  | jq -r '.certificateArn'`
+
+echo "Certificate ARN: ${certificate_arn}"
+
+echo "- - - - - - - - - - - - - - - - - - - -"
+echo "Attach Policy to generated certificate."
+
+aws iot attach-policy \
+  --policy-name ${policy_name} \
+  --target ${certificate_arn} \
+  --region ${AWS_REGION}
+
+echo "- - - - - - - - - - - - - - - - - - - -"
+echo "Attach IoT Thing to generated certificate."
+
+aws iot attach-thing-principal \
+  --thing-name ${thing_name} \
+  --principal ${certificate_arn} \
+  --region ${AWS_REGION}
+
+echo "- - - - - - - - - - - - - - - - - - - -"
+echo "Done."
